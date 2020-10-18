03b9526d0a13adaf428ce73ee180478af3a22766
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-18 15:42:42 +0530

    [Mihir] Refactor Edit contact details functionality added

--- Program.cs
+++ Program.cs
@@ -5,12 +5,42 @@ namespace AddressBook
 {
     class Program
     {
+        AddressBook book;
+        public Program()
+        {
+            book = new AddressBook();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Welcome to the Address Book Problem");
-            Contact.CreateContact();
-            AddressBook.AddPerson();
-
+            Program newProgram = new Program();
+            Console.WriteLine("Welcome to the Address Book Program!");
+            bool exist = true;
+            while (exist)
+            {
+                Console.WriteLine("Select the option. \n1. Add new contact. \n2. Edit existing contact. \n3. Exit.");
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        {
+                            newProgram.book.AddPerson();
+                            Console.WriteLine("Contact added!");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter the first name of that person: ");
+                            newProgram.book.EditPersonDetails();
+                            break;
+                        }
+                    case 3:
+                        {
+                            exist = false;
+                            break;
+                        }
+                }
+            }
         }
     }
 }
+
